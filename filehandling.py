092a828cf8a18faf3430232975eb91a5fd314baf
Kanchan-bhambere <kanchanbhambere1999@gmail.com>
2024-12-10 11:23:14 +0530

    adding my daily practice

---
+++ filehandling.py
@@ -0,0 +1,5 @@
+f= open("D:\ToDoApp\Python-coding\overloading and overriding.py", "r")
+i=1
+while i<=10:
+    print(f.readline())
+    i+=1
--- overloading and overriding.py
+++ overloading and overriding.py
@@ -1,5 +1,6 @@
 # Overloading
 
+"""
 obj=ABC()
 obj.Disp()
 obj.Disp("kanchan")
@@ -14,3 +15,27 @@ obj=ABCD()
 obj.Show( )
 obj.Show('kanchan')
 obj.Show('kanchan', 'bhambere')
+
+
+def checkTriplet(array):
+    n = len(array)
+    for i in range(n):
+        array[i] = array[i]**2
+
+    array.sort()
+
+    for i in range(n - 1, 1, -1):
+        s = set()
+        for j in range(i - 1, -1, -1):
+            if (array[i] - array[j]) in s:
+                return True
+            s.add(array[j])
+    return False
+
+
+arr = [3, 2, 4, 6, 5]
+a=checkTriplet(arr)
+print(a)"""
+# True
+
+
